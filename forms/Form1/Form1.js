c747e9c6e97ff3cf3a2df3ad6ff98f50d6fb9092
Devinmartini <60595569+Devinmartini@users.noreply.github.com>
2020-02-03 14:58:35 -0600

    Update Form1.js

--- forms/Form1/Form1.js
+++ forms/Form1/Form1.js
@@ -4,5 +4,6 @@ alert(`My best freind is ${names[1]}`)
 
 btnShowMessage.onclick=function(){
   alert(`My best freind is ${names[1]}`)
-  lblMessage.textContent = "I really enjoy" + names[1]
+  alert(`The first person in the array is ${names[0]}.`)
+  lblMessage.textContent = "I really enjoy " + names[1]
 }
